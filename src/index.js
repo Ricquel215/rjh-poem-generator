cce5eb6fa5e09fab67c7b27f3545b0deb1f834ba
Ricquel215 <ricqueljh215@gmail.com>
2025-04-05 19:12:14 -0500

    Made change to string

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: "I love to travel the world in luxury",
     autoStart: true,
     delay: 1,
     cursor: "",
